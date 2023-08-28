50ccfc7c4a85debe0ba1e41653f2b47572aabc7c
KOVALW <wkoval@uchicago.edu>
2023-08-28 16:25:08 -0500

    Updated cleaning to include gene key

--- data/processing_code/consistency_checks.R
+++ data/processing_code/consistency_checks.R
@@ -20,5 +20,38 @@ raw_count_data %>%
   write_csv("./data/data_cleaned/verify_linenumber_table.csv")
 
 #one mismatch identified - now checks out
+driver_lede_key <- raw_count_data %>% 
+  group_by(driver) %>% 
+  summarise(o=n()) %>% 
+  filter(!is.na(driver)) %>% 
+  select(-o) %>% 
+  mutate(driver_lede = substr(driver,1,3))
+
 raw_count_data %>% mutate(r=row_number()) %>% 
   filter(offspring_male + offspring_female != offspring_total)
+
+#Recreate experiment key-----
+experiment_list <- gene_driver_pair %>% 
+  select(construct_line, gene_name,ends_with("library")) %>% 
+  gather(-construct_line, -gene_name, key = "driver_lede", value = "libraries_used") %>% 
+  mutate(driver_lede = substr(driver_lede, 1,3),
+         GD = grepl("GD", libraries_used),
+         KK = grepl("KK", libraries_used)) %>% 
+  select(-libraries_used) %>% 
+  gather(-construct_line, -gene_name, -driver_lede, key = "background_library", value = "used") %>% 
+  filter(used) %>% 
+  left_join(driver_lede_key,
+            by = "driver_lede") %>% 
+  select(-used, -driver_lede)
+  
+observed_experiments <- cleaned_count_data %>% 
+  group_by(construct_line, gene_name, background_library, driver) %>% 
+  summarise(obs= n(),
+            ) %>% 
+  ungroup() 
+
+observed_experiments %>% 
+  arrange(construct_line, background_library, driver)
+
+experiment_list %>% 
+  arrange(construct_line, background_library, driver)
--- data/processing_code/data_cleaning.R
+++ data/processing_code/data_cleaning.R
@@ -2,8 +2,10 @@ library(tidyverse)
 library(lubridate)
 
 raw_count_data <- read_csv("./data/data_raw/final_count_data.csv", guess_max = 5e4)
+gene_key <- read_csv("./data/data_raw/gene_key.csv")
+gene_driver_pair <- read_csv("data/data_raw/experiment_key.csv")
 
-#Making library and line consistent-----
+#Cleaning library and line data values-----
 #should coerce some values to NA
 libraries_incl <- c("GD","KK")
 
@@ -49,8 +51,23 @@ raw_count_data <- raw_count_data %>%
     cross_line_name
   ))
 
+#Pairing to gene name key-----
+gene_key <- gene_key %>% 
+  mutate(background_library = substr(vdrc_id, 
+                                     nchar(vdrc_id) - 1, 
+                                     nchar(vdrc_id))) %>% 
+  mutate(background_library = ifelse(!background_library %in% libraries_incl,
+                                      "KK", background_library))
+
+
+cleaned_count_data <- raw_count_data %>% 
+  select(background_library, background_line, driver, construct_line, sex_test, count_date, bio_replicate, starts_with("offspring"), food_abnormality, parental_mortality, driver_present, rnai_construct_present) %>% 
+  full_join(gene_key,
+            by = c("background_library", "construct_line"))
+  
+
 #Computer-friendly variables-----
-raw_count_data <- raw_count_data %>% 
+cleaned_count_data <- cleaned_count_data %>% 
   mutate(count_date = as.Date(count_date, format = "%m/%d/%Y")) %>% 
   mutate(count_month = month(count_date)) %>% 
   mutate(sex_test = ifelse(sex_test %in% c("808", "60100"), 
@@ -66,7 +83,7 @@ raw_count_data <- raw_count_data %>%
   
 
 #Final input data -----
-processed_data <- raw_count_data %>% 
+processed_data <- cleaned_count_data %>% 
   select(background_id, driver_id, uniq_construct_id,
          sex_id, parental_mortality, month_id, 
          driver_present, rnai_construct_present,
